0ae4222f418e7e4b7603ed19ca014218b2772127
osborne7 <eleanor.c.osborne@gmail.com>
2020-03-07 16:40:52 -0500

    Final organization/cleanup

--- js/app.js
+++ js/app.js
@@ -1,64 +1,41 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-const h2s = document.querySelectorAll('h2');
-
-
-
-// for each section, 
+//  for each section, 
 //     1 add an li to the ul in navlist
 //     2 get the h2's data attribute from each section in turn, in order to create the text for the li
 //     3 append the li to the ul so it shows up in navlist
 
-
+const h2s = document.querySelectorAll('h2');
 let i = 1;
 
 for (const h2 of h2s) {
+    // add an li to the nav's ul for each section h2 & get h2 text from data attribute
+    const navList = document.querySelector('#navbar__list');
+    const createLi = document.createElement('li');
+    const h2Text = h2.getAttribute('data-nav');
+    createLi.textContent = h2Text;
 
-        // add an li for each section h2 to the ul in nav
-        const navList = document.querySelector('#navbar__list');
-        const createLi = document.createElement('li');
-        const h2Text = h2.getAttribute('data-nav');
-        createLi.textContent = h2Text;
+    // link from nav to each page section
+    const link = document.createElement('a');
+    link.setAttribute('href', ('#section' + [i]));
+    i++;
+    navList.appendChild(link);
+    link.appendChild(createLi);
 
-        // link from nav to each page section
-        const link = document.createElement('a');
-        link.setAttribute('href', ('#section' + [i]));
-        i++;
-        navList.appendChild(link);
-        link.appendChild(createLi);
-
-        // make sections collapsible and add open/close symbols
-        h2.addEventListener('click', function(){
-        let collapseDiv = this.nextElementSibling;
-        if (collapseDiv.style.display === 'block') {
-            collapseDiv.style.display = 'none';
-            h2.children[0].innerHTML = " &#10515;";
-        } else {
-            collapseDiv.style.display = 'block';
-            h2.children[0].innerHTML = ' &#10514;';
-        }
-        })
-
+    // make sections collapsible and add open/close symbols
+    h2.addEventListener('click', function(){
+    let collapseDiv = this.nextElementSibling;
+    if (collapseDiv.style.display === 'block') {
+        collapseDiv.style.display = 'none';
+        h2.children[0].innerHTML = " &#10515;";
+    } else {
+        collapseDiv.style.display = 'block';
+        h2.children[0].innerHTML = ' &#10514;';
+    }
+    })
     }
 
-/// scroll to top button functionality
+
+
+// scroll to top button functionality
 const scrollToTopButton = document.getElementById('top');
 
 // detemine whether or not to display the scroll to top button based on window position
@@ -76,12 +53,12 @@ const scrollFunction = () => {
 document.addEventListener("scroll", scrollFunction);
 
 const scrollToTop = () => {
-// set a variable for the number of pixels we are from the top of the document.
-const pixelsFromTop = document.documentElement.scrollTop || document.body.scrollTop;
+// distant from top of the document
+const distanceFromTop = document.documentElement.scrollTop || document.body.scrollTop;
 // if that number is greater than 0, scroll back to 0/the top of document
-if (pixelsFromTop > 0) {
+if (distanceFromTop > 0) {
     window.requestAnimationFrame(scrollToTop);
-    window.scrollTo(0, pixelsFromTop - pixelsFromTop / 10);
+    window.scrollTo(0, distanceFromTop - distanceFromTop / 10);
 }
 };
 
@@ -92,18 +69,19 @@ scrollToTopButton.onclick = function(e) {
 }
 
 
+
 // check if element is in viewport   
 const inViewport = (element) => {
-    let bounding = element.getBoundingClientRect();
-    let elementHeight = element.offsetHeight;
-    let elementWidth = element.offsetWidth;
-
+    const bounding = element.getBoundingClientRect();
+    const elementHeight = element.offsetHeight;
+    const elementWidth = element.offsetWidth;
     return (
         bounding.top >= -elementHeight
         && bounding.left >= -elementWidth 
         && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + elementWidth
         && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + elementHeight
     )};
+
 
 
 // highlight lis when they become active
@@ -124,12 +102,13 @@ const highlightLi = () => {
 };
 
 
+
 // listen for scroll to timeout
 const pageHeader = document.querySelector('.page__header');
-let t;
+let timeout;
 document.addEventListener('scroll', function() {
-    clearTimeout(t);
-    t = setTimeout(function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
         highlightActive();
         highlightLi();
         pageHeader.style.top = "0px";
@@ -140,6 +119,7 @@ document.addEventListener('scroll', function() {
 document.onscroll = function() {
     pageHeader.style.top = "-50px";
 };
+
 
 
 // highlight sections when in viewport
@@ -158,7 +138,9 @@ const highlightActive = () => {
     }
 };
         
-// navlinks scroll to appropriate section when clicked:
+
+
+// scroll to appropriate section when navlink is clicked
 let anchorLinks = document.querySelectorAll('a[href^="#"]');
 
 for (let anchorLink of anchorLinks) {
@@ -172,24 +154,3 @@ for (let anchorLink of anchorLinks) {
         e.preventDefault();
     })
 };
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
